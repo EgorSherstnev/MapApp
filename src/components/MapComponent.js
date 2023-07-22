d36c30f4bc8d3461f814ae1795782e3f39364f43
EgorSherstnev <svarog91@list.ru>
2023-07-22 22:33:09 +0300

    added Basemaps.jsx

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css"
-import { Icon, divIcon } from "leaflet";
+//import "leaflet/dist/leaflet.css"
+import { Icon, divIcon, point } from "leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
+import Basemap from "./Basemaps";
 
 class MapComponent extends React.Component {
    state = {
       lat: 48.8566,
       lng: 2.3522,
-      zoom: 13
+      zoom: 13,
+      basemap: 'osm',
    };
+
+   onBMChange = (bm) => {
+      this.setState({
+         basemap: bm
+      })
+   }
 
    render() {
       let center = [this.state.lat, this.state.lng];
@@ -29,6 +37,13 @@ class MapComponent extends React.Component {
          },
       ]
 
+      //basemap
+      const basemapsDict = {
+         osm: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+         hot: "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+         dark: "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png",
+      }
+
       const customIcon = new Icon({
          //iconUrl: "https://cdn-icons-png.flaticon.com/128/6903/6903382.png",
          iconUrl: require('../assets/images/marker-icon.png'),
@@ -37,7 +52,9 @@ class MapComponent extends React.Component {
 
       const createCustomClusterIcon = (cluster) => {
          return new divIcon({
-            html: `<div class="cluster-icon"></div>`
+            html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`,
+            className: "custom-marker-cluster",
+            iconSize: point(33, 33, true)
          })
       }
 
@@ -45,8 +62,10 @@ class MapComponent extends React.Component {
          <MapContainer center={center} zoom={this.state.zoom} >
             <TileLayer
                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+               url={basemapsDict[this.state.basemap]}
             />
+
+            <Basemap basemap={this.state.basemap} onChange={this.onBMChange}/>
 
             <MarkerClusterGroup
                chunkedLoading
